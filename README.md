2ba8a1ed4ba7ef505212ecd20f721aa6e7fbf0a0
Artmade Studio <mouktarart@gmail.com>
2024-10-13 21:42:37 +0300

    add splash screen

--- README.md
+++ README.md
@@ -1,12 +1,22 @@
-# Tauri + React + Typescript
+# TauriV2 + Vite app starter
 
-This template should help get you started developing with Tauri, React and Typescript in Vite.
+Template starter for developing with Tauri, React and Typescript in Vite.
+
+#### frontend
+
+- Tanstack router.
+- React aria component.
+- Tailwindcss.
+- react-icons.
+- `theme-toggle` and app shell.
+
+---
+
+#### backend
+
+- `sqlx` for database query.
+- splash screen `splashscreen.html` customize initial run in `lib.rs`.
 
 ## Recommended IDE Setup
 
 - [VS Code](https://code.visualstudio.com/) + [Tauri](https://marketplace.visualstudio.com/items?itemName=tauri-apps.tauri-vscode) + [rust-analyzer](https://marketplace.visualstudio.com/items?itemName=rust-lang.rust-analyzer)
-
-## To Do
-
-rust packages install sqlx
-native windows menus
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,5 @@
+use tauri::Manager;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -7,6 +9,21 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .setup(|app| {
+            let splashscreen_window = app.get_webview_window("splashscreen").unwrap();
+            let main_window = app.get_webview_window("main").unwrap();
+            // we perform the initialization code on a new task so the app doesn't freeze
+            tauri::async_runtime::spawn(async move {
+                // initialize your app here instead of sleeping :)
+                println!("Initializing...");
+                std::thread::sleep(std::time::Duration::from_secs(2));
+                println!("Done initializing.");
+                // After it's done, close the splashscreen and display the main window
+                splashscreen_window.close().unwrap();
+                main_window.show().unwrap();
+            });
+            Ok(())
+        })
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
